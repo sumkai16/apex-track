2a003deaf65c6a2a24a7421373fabacb5e993a37
Seph-Anthony <generalgenx60@gmail.com>
2026-05-31 01:28:45 +0800

    donedone

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,14 +1,21 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
+import { Platform } from "react-native";
 
-const supabaseUrl = 'https://vaqivrymjwlnlrxsducb.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhcWl2cnltandsbmxyeHNkdWNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxNDU1NjksImV4cCI6MjA5NTcyMTU2OX0.x-lu_ld60RpW-eyteyobizyhyFSDcREXp9s0Jm-u4UM'
+const supabaseUrl = "https://vaqivrymjwlnlrxsducb.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6InZhcWl2cnltandsbmxyeHNkdWNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxNDU1NjksImV4cCI6MjA5NTcyMTU2OX0.x-lu_ld60RpW-eyteyobizyhyFSDcREXp9s0Jm-u4UM";
+
+const authOptions: any = {
+  autoRefreshToken: true,
+  persistSession: true,
+  detectSessionInUrl: false,
+};
+
+if (Platform.OS !== "web") {
+  authOptions.storage =
+    require("@react-native-async-storage/async-storage").default;
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    auth: {
-        storage: AsyncStorage,
-        autoRefreshToken: true,
-        persistSession: true,
-        detectSessionInUrl: false,
-    },
-})
+  auth: authOptions,
+});
